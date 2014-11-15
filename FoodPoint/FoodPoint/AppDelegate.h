4e7b688184b1c700cb9f8eee9f430c43a1b32675
bigwangsmash <bigwangsmash@Brians-MacBook-Pro-3.local>
2014-11-15 15:23:13 -0500

    new stuff

---
+++ FoodPoint/FoodPoint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FoodPoint
+//
+//  Created by Brian Wang on 11/15/14.
+//  Copyright (c) 2014 FoodPoint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FoodPoint/FoodPoint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FoodPoint
+//
+//  Created by Brian Wang on 11/15/14.
+//  Copyright (c) 2014 FoodPoint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
